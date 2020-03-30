2939940e3233976333578ad4820b01105687f2e7
els <els@LAPTOP-7IIQL0MB>
2020-03-30 17:51:43 +0700

    Add project files.

---
+++ TheCoffeMechine/CoffeMechine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TheCoffeMechine
+{
+    class CoffeMechine
+    {
+        private CoffePowder CoffePowder;
+        private WaterGalon waterGalon;
+        private Milk Milk;
+
+        public CoffeMechine(CoffePowder powder, WaterGalon galon, Milk milk) 
+        {
+            this.waterGalon = galon;
+            this.CoffePowder = powder;
+            this.Milk = milk;
+        }
+
+        public String makeEspresso() 
+        {
+            if (!this.waterGalon.isAvailable())
+            {
+                return "Sorry, the water is empty";
+            }
+            if(!this.CoffePowder.isAvailable()) 
+            {
+                return "Sorry, the coffe is empty";
+            }
+
+            this.waterGalon.makeOneCup();
+            this.CoffePowder.makeOneCup();
+            return "Yey! your Espresso is ready";
+        }
+        public String makeCappucino()
+        {
+            if (!this.waterGalon.isAvailable())
+            {
+                return "Sorry, the water is empty";
+            }
+            if (!this.CoffePowder.isAvailable())
+            {
+                return "Sorry, the coffe is empty";
+            }
+            if (!this.CoffePowder.isAvailable())
+            {
+                return "Sorry, the milk is empty";
+            }
+
+            this.waterGalon.makeOneCup();
+            this.CoffePowder.makeOneCup();
+            this.Milk.makeOneCup();
+            return "Yey! your Cappucino is ready";
+        }
+        public String checkAvailability() {
+            return $"the water :{this.waterGalon.getVolume()} and the coffe powder : {this.CoffePowder.getNetto()}, and the milk : {this.Milk.getVolume()}";
+        }
+    }
+}
---
+++ TheCoffeMechine/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TheCoffeMechine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoffePowder coffe = new CoffePowder(100);
+            WaterGalon water = new WaterGalon(1000);
+            Milk milk = new Milk(200);
+            CoffeMechine yummyCoffe = new CoffeMechine(coffe, water, milk);
+
+            //1
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            String result = yummyCoffe.makeCappucino();
+            Console.WriteLine("result " + result);
+
+            //2
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeEspresso();
+            Console.WriteLine("result " + result);
+
+            //3
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeEspresso();
+            Console.WriteLine("result " + result);
+
+            //4
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeCappucino();
+            Console.WriteLine("result " + result);
+
+            //5
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeCappucino();
+            Console.WriteLine("result " + result);
+
+            //6
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+            result = yummyCoffe.makeEspresso();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("check " + yummyCoffe.checkAvailability());
+        }
+    }
+}
